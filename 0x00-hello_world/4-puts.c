82091c5562892a0510265f241a24cdf4edf98028
SamarMI <samar.mohamed.5523@gmail.com>
2023-04-21 00:40:31 +0200

    task v2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include<stdio.h>
+/**
+ * main - prints string using puts function
+ * Return: 0 if exited properly, non-zero otherwise
+*/
 int main(void)
 {
     puts("Programming is like building a multilingual puzzle\n");
-
     return(0);
 }
