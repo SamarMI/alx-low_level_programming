52ebe58098baa362b84cbf1dcd6489952e7c5c9b
SamarMI <samar.mohamed.5523@gmail.com>
2023-04-21 01:20:44 +0200

    task v5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,12 @@
 #include<stdio.h>
+
 /**
  * main - prints string using printf function
  * Return: 0 if exited properly, non-zero otherwise
 */
 int main(void)
 {
-printf ("with proper grammar, but the outcome is a piece of art,\n");
-return(0);
+    printf ("with proper grammar, but the outcome is a piece of art,\n");
+
+    return(0);
 }
