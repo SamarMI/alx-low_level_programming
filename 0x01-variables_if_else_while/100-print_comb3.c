b8f45442d8fa332504e277b25354769a41680d9b
SamarMI <samar.mohamed.5523@gmail.com>
2023-04-24 20:21:14 +0200

    add advance task1

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main is prints all possible different combinations of two digit
+ *  followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n;
+	int m;
+	int k;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		for (m = n+1; m<'9' ;m++)
+		{
+			putchar(n);
+			putchar(m);
+			if (n != '8' && m != '9')
+			{
+				putchar(',');
+				putchar(' ');
+
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+
+}
