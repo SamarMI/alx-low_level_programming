85ecee7bd147098c4f0666f98ae90ef68e86431f
SamarMI <samar.mohamed.5523@gmail.com>
2023-04-24 18:41:17 +0200

    add task files v-1

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,8 +3,8 @@
 #include <stdio.h>
 
 /**
- * main - Prints the last digit of a randomly generated number
- *        and whether it is greater than 5, less than 6, or 0.
+ * main - Prints the randomly generated numberis positive
+ *        or negitive or 0.
  *
  * Return: Always 0.
  */
@@ -14,11 +14,11 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n % 10 > 0)
+	if (n > 0)
 	{
 		printf("%d is positive\n", n);
 	}
-	else if (n % 10 == 0)
+	else if (n == 0)
 	{
 		printf("%d is zero\n", n);
 	}
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main -  prints the in lowercase, and then in uppercase
+ *  followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+
+}
