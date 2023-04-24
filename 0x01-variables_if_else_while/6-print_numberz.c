fed146235c9969bbcde8c01901181ffd48bfb10b
SamarMI <samar.mohamed.5523@gmail.com>
2023-04-24 19:55:56 +0200

    add all files task

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main -prints all single digit numbers of base 10 starting from 0
+ *  followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+
+}
