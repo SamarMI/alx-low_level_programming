b55d487fe5cdc55de36e2e9df7396d90ec7488a4
SamarMI <samar.mohamed.5523@gmail.com>
2023-04-25 19:53:55 +0200

    add files after compilation

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -17,7 +17,7 @@ void print_to_98(int n)
 		{
 			if (i != 98)
 			{
-				printf("%d ,", i);
+				printf("%d, ", i);
 			}
 			else
 			{
@@ -32,7 +32,7 @@ void print_to_98(int n)
 		{
 			if (i != 98)
 			{
-				printf("%d ,", i);
+				printf("%d, ", i);
 			}
 			else
 			{
