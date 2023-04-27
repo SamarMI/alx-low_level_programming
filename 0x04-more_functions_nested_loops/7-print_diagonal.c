60ddb2923d114c7eb951a91b5c9019e3ecb94d07
SamarMI <samar.mohamed.5523@gmail.com>
2023-04-28 02:20:00 +0300

    edit tasks

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,16 +12,16 @@ void print_diagonal(int n)
 	int j;
 
 	if (n <= 0)
-    {
+	{
 		_putchar('\n');
-    }
+	}
 	for (i = 0; i < n; i++)
 	{
 		for (j = 0; j < i; j++)
 		{
 			_putchar(' ');
 		}
-        _putchar('\\');
+		_putchar('\\');
 		_putchar('\n');
 	}
 
