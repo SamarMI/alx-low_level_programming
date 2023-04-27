96535ca6e8f69c052e790c7bab6173adcd4e6394
SamarMI <samar.mohamed.5523@gmail.com>
2023-04-27 20:20:01 +0200

    add task-0

---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+0x03-debugging for debugging problem
---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints the randomly generated numberis positive
+ *        or negitive or 0.
+ *
+ * Return: Always 0.
+ */
+int positive_or_negative(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+	    printf("%d is negative\n", n);
+	}
+	return (0);
+
+}
