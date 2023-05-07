c55d59d419a4fdb89525a0f7867fd722436046eb
SamarMI <samar.mohamed.5523@gmail.com>
2023-05-07 23:50:43 +0300

    add tasks

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * factorial - function that returns the factorial of a given number.
+ *
+ * @n: number to get factorial of it.
+ * Return: the factorial of number.
+ */
+int factorial(int n)
+{
+	if (n == 1 || n == 0)
+	{
+		return (1);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	return (n * factorial(n - 1));
+
+}
---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - function that returns
+ * the value of x raised to the power of y.
+ * @x: integer to be raised to the power y.
+ * @y: the power which will be rasised.
+ * Return: returns the value of x raised to the power of y.
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		y--;
+		return (x * _pow_recursion(x ,y));
+	}
+
+}
