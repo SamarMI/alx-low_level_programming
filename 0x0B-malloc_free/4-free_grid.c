ecdce26fd0c5fafdcf74852e19abe526a21ad084
SamarMI <samar.mohamed.5523@gmail.com>
2023-05-15 16:31:19 +0300

    add task4

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -9,17 +9,13 @@
  */
 void free_grid(int **grid, int height)
 {
-	int  i, j;
+	int  i;
 
 	if (grid == NULL || height == 0)
 		return;
 	for (i = 0; i < height; i++)
 	{
-		for (j = 0; j <= height; j++)
-		{
-			free(grid[j]);
-		}
-
+		free(grid[i]);
 	}
 	free(grid);
 
