2a2e926708e9fb0b769b9826d68f7d3260f229f4
SamarMI <samar.mohamed.5523@gmail.com>
2023-05-24 20:22:45 +0300

    add task 4

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,9 +1,10 @@
 #include "3-calc.h"
 
 /**
- * get_op_func -  the function that selects the correct function
+ * get_op_func - the function that selects the correct function
  * to perform the operation asked by the user.
  * @s: the function  given by the user.
+ * 
  * Return: returns a pointer to the function that corresponds
  * to the operator given as a parameter.
  */
@@ -26,6 +27,6 @@ int (*get_op_func(char *s))(int, int)
 			return (ops[i].f);
 		i++;
 	}
-	return (NULL);
 
+	return (NULL);
 }
