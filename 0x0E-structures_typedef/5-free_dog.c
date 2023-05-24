bdef82b8882c2176505573dbc83c0de0d90aa53d
SamarMI <samar.mohamed.5523@gmail.com>
2023-05-24 20:28:54 +0300

    edit task

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,15 +1,16 @@
 #include <stdlib.h>
 #include "dog.h"
 /**
- * *new_dog - initialize a variable of type struct dog.
+ * free_dog - initialize a variable of type struct dog.
  * @d:pointer to struct dog
  *
  */
 void free_dog(dog_t *d)
 {
-
-	free(d->name);
-	free(d->owner);
-	free(d);
-
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
 }
