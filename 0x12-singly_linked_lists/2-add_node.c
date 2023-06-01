659a9d0024dc67a117abb368d7fe7ddc2d31407f
SamarMI <samar.mohamed.5523@gmail.com>
2023-06-01 16:17:52 +0300

    add task2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,6 +11,7 @@
  * Return: the address of the new element, or NULL if it failed.
  */
 
+/*
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
@@ -38,4 +39,24 @@ list_t *add_node(list_t **head, const char *str)
 
 	return (new_node);
 
+}
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	unsigned int len = 0;
+
+	while (str[len])
+		len++;
+
+	new = malloc(sizeof(list_t));
+	if (!new)
+		return (NULL);
+
+	new->str = strdup(str);
+	new->len = len;
+	new->next = (*head);
+	(*head) = new;
+
+	return (*head);
 }
