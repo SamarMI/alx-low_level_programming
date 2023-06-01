b8e95628458327f905c8063237b498f32e154cde
SamarMI <samar.mohamed.5523@gmail.com>
2023-06-01 16:11:10 +0300

    add task2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -13,20 +13,23 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
 
-    new_node = malloc(sizeof(list_t));
-	new_node->str = strdup(str);;
+	new_node = malloc(sizeof(list_t));
+	if (!new_node)
+		return(NULL);
+
+	new_node->str = strdup(str);
 	new_node->len = strlen(str);
 
-	if (! *head)
+	if (!*head)
 	{
-		*head = new_node; 
+		(*head) = new_node; 
 		new_node->next = NULL;
 	}
 	else
 	{
 		new_node->next = *head;
-		*head = new_node; 
-    }
+		(*head) = new_node; 
+	}
 
 	return (new_node);
 
