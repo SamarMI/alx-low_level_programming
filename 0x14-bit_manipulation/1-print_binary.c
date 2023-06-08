3f755495e4825ea4ceae3dbf0270b0da7d6aca46
SamarMI <samar.mohamed.5523@gmail.com>
2023-06-08 15:11:18 +0300

    add task1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,26 +8,45 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int shift = 1;
-    int size = 0, binary = 0;
+	// unsigned long int shift = 1;
+    // int size = 0, binary = 0;
 
-	if (!n)
-    {
-        _putchar('0');
-        return;
-    }
-    size = sizeof(n) * 8;
-	while (size)
+	// if (!n)
+    // {
+    //     _putchar('0');
+    //     return;
+    // }
+    // size = sizeof(n) * 8;
+	// while (size)
+	// {
+	// 	if (n & shift << --size)
+	// 	{
+	// 		_putchar('1');
+	// 		binary++;
+	// 	}
+	// 	else
+	// 	{
+	// 		_putchar('0');
+	// 	}
+	// }
+	unsigned long int temp;
+	int s = 0;
+
+	if (n == 0)
 	{
-		if (n & shift << --size)
-		{
-			_putchar('1');
-			binary++;
-		}
+		printf("0");
+		return;
+	}
+
+	for (temp = n; (temp >>= 1) > 0; s++)
+		;
+
+	for (; s >= 0; s--)
+	{
+		if ((n >> s) & 1)
+			printf("1");
 		else
-		{
-			_putchar('0');
-		}
+			printf("0");
 	}
 
 }
