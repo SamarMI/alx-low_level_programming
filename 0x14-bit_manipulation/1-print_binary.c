4a0db25d5dd0fbfdf69ebaac2fb9c3745f093a9e
SamarMI <samar.mohamed.5523@gmail.com>
2023-06-08 15:12:13 +0300

    add task1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -4,31 +4,10 @@
 /**
  * binary_to_uint - prints the binary representation of a number.
  * @n: integer to be printed.
- * 
+ *
  */
 void print_binary(unsigned long int n)
 {
-	// unsigned long int shift = 1;
-    // int size = 0, binary = 0;
-
-	// if (!n)
-    // {
-    //     _putchar('0');
-    //     return;
-    // }
-    // size = sizeof(n) * 8;
-	// while (size)
-	// {
-	// 	if (n & shift << --size)
-	// 	{
-	// 		_putchar('1');
-	// 		binary++;
-	// 	}
-	// 	else
-	// 	{
-	// 		_putchar('0');
-	// 	}
-	// }
 	unsigned long int temp;
 	int s = 0;
 
