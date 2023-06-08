95870834b23648f4138a6e1ea3661d3ad7a09d2a
SamarMI <samar.mohamed.5523@gmail.com>
2023-06-08 15:05:06 +0300

    add task0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -4,7 +4,7 @@
 /**
  * binary_to_uint - convert a binary number to an unsigned int
  * @b: char string which will be converted.
- * 
+ *
  * Return: converted decimal number or 0 if there is an unconvertable char
  */
 unsigned int binary_to_uint(const char *b)
@@ -17,15 +17,16 @@ unsigned int binary_to_uint(const char *b)
 
 	while (b[length])
 	{
-		if ( b[length] != 48 && b[length] != 49)
-			return(0);
+		if (b[length] != 48 && b[length] != 49)
+			return (0);
 		length++;
 	}
 
-for (length--; length >= 0; length--, power *= 2)
+	for (length--; length >= 0; power *= 2)
 	{
 		if (b[length] == 49)
 			total += power;
+		length--;
 	}
 	return (total);
 }
