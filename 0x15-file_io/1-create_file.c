f99c86828296904f0695a7b40f4536a3e421f9b3
SamarMI <samar.mohamed.5523@gmail.com>
2023-06-13 17:31:17 +0300

    edit tasks

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int open_myfile, write_myfile, letters = 0;
 
-	if (!filename | !text_content)
+	if (!filename)
 		return (-1);
 
 	while (text_content[letters])
--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,10 +3,12 @@
 #include <stdlib.h>
 
 char *create_buffer(char *file);
+void close_file(int fd);
 
 /**
  * create_buffer - Allocates 1024 bytes for a buffer.
  * @file: The name of the file buffer is storing chars for.
+ *
  * Return: A pointer to the newly-allocated buffer.
  */
 char *create_buffer(char *file)
@@ -25,6 +27,22 @@ char *create_buffer(char *file)
 	return (buffer);
 }
 
+/**
+ * close_file - Closes file descriptors.
+ * @fd: The file descriptor to be closed.
+ */
+void close_file(int fd)
+{
+	int c;
+
+	c = close(fd);
+
+	if (c == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
 
 /**
  * main - Copies the contents of a file to another file.
@@ -34,61 +52,52 @@ char *create_buffer(char *file)
  * Return: 0 on success.
  *
  * Description: If the argument count is incorrect - exit code 97.
- *              If file_src does not exist or cannot be read - exit code 98.
+ *              If file_from does not exist or cannot be read - exit code 98.
  *              If file_to cannot be created or written to - exit code 99.
- *              If file_to or file_src cannot be closed - exit code 100.
+ *              If file_to or file_from cannot be closed - exit code 100.
  */
 int main(int argc, char *argv[])
 {
-	int src, dist, r, w, c1, c2;
+	int from, to, r, w;
 	char *buffer;
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_src file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
+
 	buffer = create_buffer(argv[2]);
-	src = open(argv[1], O_RDONLY);
-	r = read(src, buffer, 1024);
-	dist = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	from = open(argv[1], O_RDONLY);
+	r = read(from, buffer, 1024);
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	do {
-		if (src == -1 || r == -1)
+		if (from == -1 || r == -1)
 		{
 			dprintf(STDERR_FILENO,
-				"Error: Can't read src file %s\n", argv[1]);
+				"Error: Can't read from file %s\n", argv[1]);
 			free(buffer);
 			exit(98);
 		}
 
-		w = write(dist, buffer, r);
-		if (dist == -1 || w == -1)
+		w = write(to, buffer, r);
+		if (to == -1 || w == -1)
 		{
 			dprintf(STDERR_FILENO,
-				"Error: Can't write dist %s\n", argv[2]);
+				"Error: Can't write to %s\n", argv[2]);
 			free(buffer);
 			exit(99);
 		}
 
-		r = read(src, buffer, 1024);
-		dist = open(argv[2], O_WRONLY | O_APPEND);
+		r = read(from, buffer, 1024);
+		to = open(argv[2], O_WRONLY | O_APPEND);
 
 	} while (r > 0);
 
 	free(buffer);
-	c1 = close(src);
-	if (c1 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close c1 %d\n", c1);
-		exit(100);
-	}
-	c2 = close(dist);
-	if (c2 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close c2 %d\n", c2);
-		exit(100);
-	}
+	close_file(from);
+	close_file(to);
 
 	return (0);
 }
